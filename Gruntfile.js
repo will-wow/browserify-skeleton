115443475dbd4d2ae937aaada7ebf46f045cb8c1
Will <will@whentheresawill.net>
2016-03-02 05:18:55 -0800

    fix a naming bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
 		'clean:dev',
 		'sass:dev',
 		'browserify:dev',
-		'copy:dev',
+		'sync:dev',
 		'connect',
 		'watch'
 	]);
--- source/greetings/index.js
+++ source/greetings/index.js
@@ -4,7 +4,7 @@ var angular = require('angular');
 
 var mod = angular.module('app.greetings', []);
 
-mod.controller('GreetingController', require('./greetings-controller'));
+mod.controller('GreetingsController', require('./greetings-controller'));
 mod.factory('greetings', require('./greetings-service'));
 mod.directive('greeting', require('./greeting-directive'));
 
